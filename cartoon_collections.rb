9e9bfa182efa6ce8af215b0a4063976c0c80e884
witty-trace-1906 <menhajuddin.sharaf@gmail.com>
2020-12-28 14:46:23 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,6 +12,6 @@ def list_dwarves(array)
   #
   # Print a numbered list of each element
   array.each_with_index do |value, index|
-    puts "#{index} #{value}"
+    puts "The index #{index} of #{value}"
   end
 end
