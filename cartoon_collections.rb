4251a910f510edebbd1fdabcb3b6008d44d59692
witty-trace-1906 <menhajuddin.sharaf@gmail.com>
2020-12-28 14:40:00 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,10 +2,16 @@ def greet_characters(array)
   # Use `each` to enumerate over the provided array
   #
   # Print a custom greeting for each element
+  array.each do |name|
+    puts "#{name}, how are?"
+  end
 end
 
 def list_dwarves(array)
   # Use `each_with_index` to enumerate over the provided array
   #
   # Print a numbered list of each element
+  array.each_with_index do |value, index|
+    puts "#{index} #{value}"
+  end
 end
